9bd9e766adda54916a6a65393f035a11666bc489
simpleauthority <jacob@algorithmjunkie.com>
2020-10-15 00:12:44 -0700

    spacing

--- components/portfolio/PortfolioEntry.vue
+++ components/portfolio/PortfolioEntry.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-col cols="12" lg="6" xl="4">
+  <b-col cols="12" lg="6" xl="4" class="entry-container">
     <b-card
       :title="title"
       :img-src="image"
@@ -57,60 +57,63 @@ export default {
 </script>
 
 <style lang="scss">
-.portfolio-entry {
-  min-height: 575px;
-  max-height: 575px;
-  margin-bottom: 10px;
+.entry-container {
+  margin-bottom: 15px;
 
-  @media (min-width: 576px) {
-    min-height: 560px;
-    max-height: 560px;
-  }
+  .portfolio-entry {
+    min-height: 575px;
+    max-height: 575px;
 
-  @media (min-width: 768px) {
-    min-height: 580px;
-    max-height: 580px;
-  }
+    @media (min-width: 576px) {
+      min-height: 560px;
+      max-height: 560px;
+    }
 
-  @media (min-width: 1200px) {
-    min-height: 610px;
-    max-height: 610px;
-  }
+    @media (min-width: 768px) {
+      min-height: 580px;
+      max-height: 580px;
+    }
 
-  .card-img-top {
-    height: 14rem;
-  }
+    @media (min-width: 1200px) {
+      min-height: 610px;
+      max-height: 610px;
+    }
 
-  .card-text {
-    p {
-      font-size: 0.95rem;
+    .card-img-top {
+      height: 14rem;
+    }
 
-      @media (min-width: 576px) {
-        font-size: 1.05rem;
-      }
+    .card-text {
+      p {
+        font-size: 0.95rem;
 
-      @media (min-width: 768px) {
-        font-size: 1.1rem;
-      }
+        @media (min-width: 576px) {
+          font-size: 1.05rem;
+        }
 
-      @media (min-width: 992px) {
-        font-size: 1.1rem;
-      }
+        @media (min-width: 768px) {
+          font-size: 1.1rem;
+        }
 
-      @media (min-width: 1200px) {
-        font-size: 1.12rem;
+        @media (min-width: 992px) {
+          font-size: 1.1rem;
+        }
+
+        @media (min-width: 1200px) {
+          font-size: 1.12rem;
+        }
       }
     }
-  }
 
-  .card-footer {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-  }
-
-    .link-text {
-      margin-top: 1rem;
+    .card-footer {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
     }
+
+      .link-text {
+        margin-top: 1rem;
+      }
+  }
 }
 </style>
