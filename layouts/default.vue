bf0f02d39f2cb4885fd01bdcb97130908c1ba7ef
Jacob Andersen <jacob@algorithmjunkie.com>
2022-11-08 01:47:05 -0800

    add mastodon verification code to layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,6 @@
     <main>
       <nuxt />
     </main>
+    <a rel="me" href="https://social.jacobandersen.dev/@simple" style="display:none;">Mastodon</a>
   </b-container>
 </template>
--- layouts/page.vue
+++ layouts/page.vue
@@ -5,6 +5,8 @@
     <main>
       <nuxt />
     </main>
+
+    <a rel="me" href="https://social.jacobandersen.dev/@simple" style="display:none;">Mastodon</a>
   </b-container>
 </template>
 
