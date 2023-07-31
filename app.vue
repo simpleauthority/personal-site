492f9018c7aec37577eb44599ba22d4d065708a7
Jacob Andersen <jacob@algorithmjunkie.com>
2023-07-31 13:43:20 +0800

    updates

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout class="h-screen">
     <NuxtPage />
-    <a rel="me" href="https://social.jacobandersen.dev/@simple" style="display:none;">Mastodon</a>
+    <a rel="me" href="https://mastodon.social/@simpleauthority" style="display:none;">Mastodon</a>
   </NuxtLayout>
 </template>
