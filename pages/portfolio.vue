925b53583097d08cb84b1730b1eab82a6097c00b
Jacob Andersen <jacob@algorithmjunkie.com>
2023-08-02 15:14:55 +0800

    add missing pages

---
+++ pages/portfolio.vue
@@ -0,0 +1,18 @@
+<script setup>
+useHead({
+  title: 'Jacob Andersen\'s Resume',
+  meta: [
+    {
+      name: 'description',
+      content: 'Computer Scientist in progress'
+    }
+  ]
+})
+</script>
+
+<template>
+  <div class="h-screen flex flex-col items-center justify-center pb-5">
+    <h1>You caught me. There's nothing here yet.</h1>
+    <nuxt-link to="/">Go back.</nuxt-link>
+  </div>
+</template>
